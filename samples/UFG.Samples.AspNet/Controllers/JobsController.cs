33dd7761db45d524ace7934480955c320dd5de7d
pjwilliams2 <williams.patrick.j@gmail.com>
2022-10-29 13:57:13 -0400

    Refine aspnet sample

--- samples/UFG.Samples.AspNet/Controllers/JobsController.cs
+++ samples/UFG.Samples.AspNet/Controllers/JobsController.cs
@@ -10,12 +10,13 @@ public class JobsController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> EnqueueJob([FromServices] ITaskQueue queue)
     {
-        await queue.EnqueueAsync((scope, ct) =>
+        await queue.EnqueueAsync(async (scope, ct) =>
         {
+            //Do time consuming work....
+            await Task.Delay(10_000, ct);
+            
             var logger = scope.ServiceProvider.GetRequiredService<ILogger<JobsController>>();
             logger.Log(LogLevel.Information, "Logging from enqueued task");
-
-            return Task.CompletedTask;
         });
 
         return NoContent();
